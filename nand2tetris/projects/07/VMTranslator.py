de5b6ecd322a645cd465c4e06f1a7638e7842dc5
Nilson Cesar <admncoaf@gmail.com>
2023-10-29 10:19:24 -0300

    Implement arithmetic methods in CodeWriter

--- nand2tetris/projects/07/VMTranslator.py
+++ nand2tetris/projects/07/VMTranslator.py
@@ -40,6 +40,16 @@ class CodeWriter():
                 self.asm_code += self.neg_asm.code()
             if vm_operation[0] == "eq":
                 self.asm_code += self.eq_asm_code(len(self.asm_code) + 1)
+            if vm_operation[0] == "gt":
+                self.asm_code += self.gt_asm_code(len(self.asm_code) + 1)
+            if vm_operation[0] == "lt":
+                self.asm_code += self.lt_asm_code(len(self.asm_code) + 1)
+            if vm_operation[0] == "and":
+                self.asm_code += self.and_asm_code()
+            if vm_operation[0] == "or":
+                self.asm_code += self.or_asm_code()
+            if vm_operation[0] == "not":
+                self.asm_code += self.not_asm_code()
 
     def add_asm_code(self):
         return ["@SP", "M=M-1", "A=M", "D=M", "@SP", "M=M-1", "A=M", "M=M+D", "@SP", "M=M+1"]
@@ -52,6 +62,21 @@ class CodeWriter():
 
     def eq_asm_code(self, act_line):
         return ["@SP", "M=M-1", "A=M", "D=M", "@SP", "M=M-1", "A=M", "D=M-D", f"@{13 + act_line}", "D;JEQ", "D=0", f"@{14 + act_line}", "0;JMP", "D=1", "@SP", "A=M", "M=D", "@SP", "M=M+1"]
+
+    def gt_asm_code(self, act_line):
+        return ["@SP", "M=M-1", "A=M", "D=M", "@SP", "M=M-1", "A=M", "D=M-D", f"@{13 + act_line}", "D;JGT", "D=0", f"@{14 + act_line}", "0;JMP", "D=1", "@SP", "A=M", "M=D", "@SP", "M=M+1"]
+    
+    def lt_asm_code(self, act_line):
+        return ["@SP", "M=M-1", "A=M", "D=M", "@SP", "M=M-1", "A=M", "D=M-D", f"@{13 + act_line}", "D;JLT", "D=0", f"@{14 + act_line}", "0;JMP", "D=1", "@SP", "A=M", "M=D", "@SP", "M=M+1"]
+
+    def and_asm_code(self):
+        return ["@SP", "M=M-1", "A=M", "D=M", "@SP", "M=M-1", "A=M", "M=M&D", "@SP", "M=M+1"]
+
+    def or_asm_code(self):
+        return ["@SP", "M=M-1", "A=M", "D=M", "@SP", "M=M-1", "A=M", "M=M|D", "@SP", "M=M+1"]
+
+    def neg_asm_code(self):
+        return ["@SP", "M=M-1", "A=M", "M=!M", "@SP", "M=M+1"]
 
 if __name__ == "__main__":
     file_path = sys.argv[1]

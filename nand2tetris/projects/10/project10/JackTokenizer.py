482126acd655764732f02223eb9bd6d224ca9dd1
Nilson Cesar <admncoaf@gmail.com>
2023-11-18 16:18:35 -0300

    Remove sys library in JackTokenizer

--- nand2tetris/projects/10/project10/JackTokenizer.py
+++ nand2tetris/projects/10/project10/JackTokenizer.py
@@ -1,5 +1,3 @@
-import sys
-
 class JackTokenizer:
     keywords = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
     symbols = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '>', '<', '=', '~']

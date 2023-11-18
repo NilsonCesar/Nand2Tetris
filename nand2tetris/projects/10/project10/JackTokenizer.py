c772d4a5a428d6919060e8b16afabd4e49cc422b
Nilson Cesar <admncoaf@gmail.com>
2023-11-18 16:18:05 -0300

    Refactor and resolve problems in JackTokenizer

--- nand2tetris/projects/10/project10/JackTokenizer.py
+++ nand2tetris/projects/10/project10/JackTokenizer.py
@@ -1,6 +1,8 @@
+import sys
+
 class JackTokenizer:
     keywords = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
-    symbol = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+    symbols = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '>', '<', '=', '~']
 
     def __init__(self, input):
         self.lines = self.format_input(input)
@@ -13,6 +15,11 @@ class JackTokenizer:
 
             if slash_idx != -1:
                 ans = line[:slash_idx]
+            
+            slash_idx = line.find("/**")
+            if slash_idx != -1:
+                ans = ""
+
             ans = ans.strip()
 
             if ans != "":
@@ -25,7 +32,7 @@ class JackTokenizer:
     def type_of(self, token):
         if token in JackTokenizer.keywords:
             return 'keyword'
-        if token in JackTokenizer.symbol:
+        if token in JackTokenizer.symbols:
             return 'symbol'
         if token[0] == '"':
             return 'stringConstant'
@@ -34,9 +41,19 @@ class JackTokenizer:
             return 'integerConstant'
         except:
             return 'identifier'
-    
+
+    def isInReservedWords(self, word):
+        return (word in JackTokenizer.keywords) or (word in JackTokenizer.symbols)
+
     def format_token(self, token):
         type_token = self.type_of(token)
+        if token == '&':
+            return '&amp;'
+        if token == '<':
+            return '&lt;'
+        if token == '>':
+            return '&gt;'
+
         if type_token in ['keyword', 'symbol', 'integerConstant', 'identifier']:
             return token
         return token[1:-1]
@@ -45,16 +62,23 @@ class JackTokenizer:
         tokens = []
         i = 0
         while i < len(line):
+            print(line)
             if line[i] == ' ':
+                i += 1
+                continue
+        
+            if self.isInReservedWords(line[i]):
+                tokens.append(self.make_token(line[i]))
                 i += 1
                 continue
 
             if line[i] == '"':
-                string = ''
                 i += 1
-                while line[i] != '"':
+                string = ''
+                while i < len(line) and line[i] != '"':
                     string += line[i]
                     i += 1
+                i += 1
                 tokens.append(self.make_token('"'+ string + '"'))
                 continue
             else:
@@ -73,13 +97,19 @@ class JackTokenizer:
                     continue
                 except:
                     0
-            if self.isInReservedWords(line[i]):
-                tokens.append(self.make_token(line[i]))
+            word = line[i]
+            i += 1
+            while i < len(line) and not (self.isInReservedWords(line[i]) or line[i] == ' '):
+                word += line[i]
                 i += 1
-            else:
-                identifier = ''
-                i += 1
-                while i < len(line) and not (self.isInReservedWords(line[i]) or line[i] == ' '):
-                    identifier += line[i]
-                    i += 1
-                tokens.append(self.make_token(identifier))
+                if self.isInReservedWords(word):
+                    break
+
+            tokens.append(self.make_token(word))
+        return tokens
+    
+    def tokenize(self):
+        self.program_tokenized = []
+        for line in self.lines:
+            self.program_tokenized += self.tokenize_line(line)
+        return self.program_tokenized

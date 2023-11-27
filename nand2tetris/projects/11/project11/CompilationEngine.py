bd005d6e5d6adb3ba5a84257f33ebf9b9b5eff8b
Nilson Cesar <admncoaf@gmail.com>
2023-11-27 17:57:52 -0300

    Create compileParameterList method in CompilationEngine

--- nand2tetris/projects/11/project11/CompilationEngine.py
+++ nand2tetris/projects/11/project11/CompilationEngine.py
@@ -14,9 +14,15 @@ class CompilationEngine:
 
     def multAdvance(self, n):
         self.act_token = self.act_token + n
-    
+
     def getCurrentToken(self):
         return self.tokens[self.act_token]
+
+    def getCurrentTokenType(self):
+        token = self.getCurrentToken()
+        first_lt = token.find('<')
+        first_gt = token.find('>')
+        return token[first_lt + 1 : first_gt]
 
     def getCurrentTokenValue(self):
         token = self.getCurrentToken()
@@ -37,10 +43,25 @@ class CompilationEngine:
         self.advance()
         return [name, type, kind]
 
+    def createParameterVariable(self):
+        kind = 'ARG'
+        type = self.getCurrentTokenValue()
+        self.advance()
+        name = self.getCurrentTokenValue()
+        self.advance()
+        return [name, type, kind]
+
     def compileClassVarDec(self):
         self.advance()
         variable = self.createClassVariable()
         self.symbol_table.define(variable[0], variable[1], variable[2])
         self.multAdvance(3)
-        
 
+    def compileParameterList(self):
+        self.advance()
+        while self.getCurrentTokenType() != '/parameterList':
+            variable = self.createParameterVariable()
+            self.symbol_table.define(variable[0], variable[1], variable[2])
+            if self.getCurrentTokenValue() == ',':
+                self.advance()
+        self.advance()

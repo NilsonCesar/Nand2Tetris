c3506547e236a08a2b6a3cfba19847a92222ebcc
Nilson Cesar <admncoaf@gmail.com>
2023-11-28 08:27:22 -0300

    Create helper function to define variables in symbol table

--- nand2tetris/projects/11/project11/CompilationEngine.py
+++ nand2tetris/projects/11/project11/CompilationEngine.py
@@ -9,6 +9,9 @@ class CompilationEngine:
         self.symbol_table = SymbolTable.SymbolTable()
         self.act_token = 0
     
+    def populeSymbolTable(self, variable):
+        self.symbol_table.define(variable[0], variable[1], variable[2])
+
     def advance(self):
         self.act_token += 1
 
@@ -60,14 +63,14 @@ class CompilationEngine:
     def compileClassVarDec(self):
         self.advance()
         variable = self.createClassVariable()
-        self.symbol_table.define(variable[0], variable[1], variable[2])
+        self.populeSymbolTable(variable)
         self.multAdvance(3)
 
     def compileParameterList(self):
         self.advance()
         while self.getCurrentTokenType() != '/parameterList':
             variable = self.createParameterVariable()
-            self.symbol_table.define(variable[0], variable[1], variable[2])
+            self.populeSymbolTable(variable)
             if self.getCurrentTokenValue() == ',':
                 self.advance()
         self.advance()
@@ -77,9 +80,9 @@ class CompilationEngine:
         type = self.getCurrentTokenValue()
         self.advance()
         variable = self.createVarVariable(type)
-        self.symbol_table.define(variable[0], variable[1], variable[2])
+        self.populeSymbolTable(variable)
         while self.getCurrentTokenValue() != ';':
             self.advance()
             variable = self.createVarVariable(type)
-            self.symbol_table.define(variable[0], variable[1], variable[2])
+            self.populeSymbolTable(variable)
         self.multAdvance(2)
